077dd4f219f809b69bdf5bffcbf014116a30e370
flarespeed <holdenbockdev@gmail.com>
2026-03-29 17:21:16 -0700

    basic market models

--- Market_API/Market/models.py
+++ Market_API/Market/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+	item_name = models.CharField(max_length=128)
+	description = models.CharField(max_length=1280)
+
+class Listing(models.Model):
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	price = models.IntegerField(default=1000)
+
+
+
